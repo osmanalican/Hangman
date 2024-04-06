178baaee069c6d456e85b2a60ffd942865840db6
osman <osman.alican@panates.com>
2024-04-07 01:42:54 +0300

    refactor:(HangmanWord.tsx): change css

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,6 +1,6 @@
 export function HangmanWord({guessedLetters, wordToGuess, reveal=false}: {guessedLetters:string[], wordToGuess:string,reveal:boolean}) {
     return (
-        <div className={"font-bold text-5xl uppercase flex gap-5"}>
+        <div className={"font-bold text-5xl uppercase flex space-x-4"}>
             {wordToGuess.split("").map((letter, index) => (
                 <span key={index} style={{borderBottom: ".1em solid black"}}>
                     <span
